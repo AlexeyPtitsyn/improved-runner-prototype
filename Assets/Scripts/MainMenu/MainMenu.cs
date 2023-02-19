d41612c6b0c46aa6deb7720f8567ad2610bc19a0
Alexey Ptitsyn <alexey.ptitsyn@gmail.com>
2023-02-19 23:36:22 +0300

    Added main menu.

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    public void OnClickStart()
+    {
+        Debug.Log("Starting a new game...");
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnClickExit()
+    {
+        Debug.Log("Exit");
+    }
+}
