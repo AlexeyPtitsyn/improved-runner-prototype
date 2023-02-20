af6812d3b8513acc9fe07887d3ffb3bb922f9c77
Alexey Ptitsyn <alexey.ptitsyn@gmail.com>
2023-02-20 22:56:27 +0300

    Improved main menu.

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -14,12 +14,17 @@ public class MainMenu : MonoBehaviour
 
     public void OnClickStart()
     {
-        Debug.Log("Starting a new game...");
+        Logger.Log("Starting a new game...");
         SceneManager.LoadScene(1);
     }
 
     public void OnClickExit()
     {
-        Debug.Log("Exit");
+        Logger.Log("Exiting the game...");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
