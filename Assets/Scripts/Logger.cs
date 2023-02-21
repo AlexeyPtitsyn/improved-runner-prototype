a373fbbaba6eba2931405d394c4b23d5b3ef3e1a
Alexey Ptitsyn <alexey.ptitsyn@gmail.com>
2023-02-21 23:51:09 +0300

    Completed logger.

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.IO;
 using UnityEngine;
 
 public static class Logger
@@ -7,8 +7,17 @@ public static class Logger
     public static void Log(string str)
     {
 #if UNITY_EDITOR
-#elif !UNITY_EDITOR && UNITY_STANDALONE
-#endif
         Debug.Log(str);
+#elif !UNITY_EDITOR && UNITY_STANDALONE
+        DateTime dt = DateTime.Now;
+        string path = Application.persistentDataPath + "/log.txt";
+        string outStr = $"{dt.ToString("[yyyy-MM-dd HH:mm:ss]")} {str}";
+
+        using (StreamWriter sw = File.AppendText(path))
+        {
+            sw.WriteLine(outStr);
+        }
+#endif
+
     }
 }
